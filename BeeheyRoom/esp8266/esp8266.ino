b3f0b7e91649e3af784afbff90e1e98faa642bd2
dehuy69 <dr.huy69@gmail.com>
2016-11-24 22:43:56 +0700

    esp8266 Post

--- BeeheyRoom/esp8266/esp8266.ino
+++ BeeheyRoom/esp8266/esp8266.ino
@@ -8,7 +8,7 @@
 const char* ssid = "Wifi";// tên wifi mà bạn muốn connect
 const char* pass = "123$%^baytamchin"; //pass wifi
 
-const char* server = "www.google.com"; //Dùng tên miền dns
+const char* server = "192.168.1.1"; //Dùng tên miền dns
 //IPAddress server(74,125,232,128); // Dùng Ip
 char url[100];
 char data[1000];
@@ -23,29 +23,51 @@ void setup() {
 }
 
 void loop() {
-
-  Serial.println(Serial.available());
-  Serial.readBytesUntil('(', url, 100);
-
-
-  Serial.readBytes(data, 1000);
-  Serial.println(data);
-  Serial.println(url);
+  if (Serial.available()) {
+    Serial.println(Serial.available());
+    Serial.readBytesUntil('(', url, 100);
 
 
-
-
-  /*
-    if (client.connect(server, 80)) {
+    Serial.readBytes(data, 100);
+    Serial.println(url);
+    Serial.println(data);
+    if (client.connect(server, 80) == 1) {
       // Make a HTTP request:
       // send the header
       sprintf(outBuf, "POST %s HTTP/1.1", url);
       client.println(outBuf);
-      sprintf(outBuf, "Host: %s", server);
+      sprintf(outBuf, "Host: %s", server);      
       client.println(outBuf);
-      client.println(F("Connection: close\r\nContent-Type: application/x-www-form-urlencoded"));
-      sprintf(outBuf, "Content-Length: %u\r\n", strlen(data));
+      client.println("User-Agent: Arduino/1.0");
+      client.println("Connection: Keep-Alive");
+      client.println("Content-Type: application/x-www-form-urlencoded; text/html; charset=utf-8");
+      sprintf(outBuf, "Content-Length: %u", strlen(data));
       client.println(outBuf);
+      client.println();
+      client.print(data);
+    }        
+  }
+  
+int connectLoop = 0;
+String inChar;
+  while (client.connected())
+  {
+    while (client.available())
+    {
+      inChar = client.readString();
+      Serial.println(inChar);
+      connectLoop = 0;
     }
-  */
+
+    delay(1);
+    connectLoop++;
+    if (connectLoop > 10000)
+    {
+      Serial.println();
+      Serial.println(F("Timeout"));
+      client.stop();
+    }
+  }
+
+   
 }
