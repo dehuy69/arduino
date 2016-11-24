a8d9a1a49cc3e89c73a2cbc2cf94baab0b520304
dehuy69 <dr.huy69@gmail.com>
2016-11-24 16:44:01 +0700

    esp8266

--- BeeheyRoom/esp8266/esp8266.ino
+++ BeeheyRoom/esp8266/esp8266.ino
@@ -7,9 +7,12 @@
 
 const char* ssid = "Wifi";// tên wifi mà bạn muốn connect
 const char* pass = "123$%^baytamchin"; //pass wifi
-const char* host = "192.168.1.43"; // ip or weburl
+
 const char* server = "www.google.com"; //Dùng tên miền dns
 //IPAddress server(74,125,232,128); // Dùng Ip
+char url[100];
+char data[1000];
+char outBuf[64];
 WiFiClient client;
 
 void setup() {
@@ -20,16 +23,29 @@ void setup() {
 }
 
 void loop() {
-  int command;
-  while (Serial.available() > 0) {
-    command = + Serial.read(); // đọc tín hiệu gửi
-  }
-  if (client.connect(server, 80)) {
-    // Make a HTTP request:
-    client.println("POST /" + String(command) + " HTTP/1.1");
-    client.println("Host: " + String(server));
-    client.println("Connection: close");
-    client.println();
-  }
 
+  Serial.println(Serial.available());
+  Serial.readBytesUntil('(', url, 100);
+
+
+  Serial.readBytes(data, 1000);
+  Serial.println(data);
+  Serial.println(url);
+
+
+
+
+  /*
+    if (client.connect(server, 80)) {
+      // Make a HTTP request:
+      // send the header
+      sprintf(outBuf, "POST %s HTTP/1.1", url);
+      client.println(outBuf);
+      sprintf(outBuf, "Host: %s", server);
+      client.println(outBuf);
+      client.println(F("Connection: close\r\nContent-Type: application/x-www-form-urlencoded"));
+      sprintf(outBuf, "Content-Length: %u\r\n", strlen(data));
+      client.println(outBuf);
+    }
+  */
 }
