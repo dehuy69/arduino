9fe29efcda9beb978fe069d46f1327c6da8d74b9
dehuy69 <dr.huy69@gmail.com>
2016-11-25 22:18:21 +0700

    flowmeter main add

--- BeeheyRoom/main/main.ino
+++ BeeheyRoom/main/main.ino
@@ -4,12 +4,18 @@ unsigned long oldTime;
 FlowMeter flowmeter;
 
 void setup() {
+  Serial.begin(9600);
   flowmeter.Setup();
 }
 
 void loop() {
   if ((millis() - oldTime) > 1000) {
     flowmeter.Loop();
+    int WaterUsed = 0;
+    if (WaterUsed = ! flowmeter.totalLitres) {
+      Serial.write("/flowmeter/(totalLitres:" + flowmeter.totalLitres);
+      WaterUsed = flowmeter.totalLitres;
+    }
   }
   oldTime = millis();
 }
